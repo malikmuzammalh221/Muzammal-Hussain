27274acdcf1dab2704cf18162bdfc6e40cdd2f83
Muzammal Hussain <malikmuzammalh221@gmail.com>
2025-04-02 15:23:25 +0500

    Add files via upload

---
+++ Models/DashboardViewModel.cs
@@ -0,0 +1,17 @@
+﻿using fortest.Context;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace fortest.Models
+{
+    public class DashboardViewModel
+    {
+        public List<Explore> ExplorModels { get; set; }
+        public List<Article> ArticleModels { get; set; }
+        public List<ClientsReview> ReviewModels { get; set; }
+        public List<HowWork> HowWorkModels { get; set; }
+    }
+}
